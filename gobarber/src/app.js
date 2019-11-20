751c629cd9901a27481b192a3761fbd9dd18b5e9
Gabriel Jony <gabjony@gmail.com>
2019-11-20 10:05:49 -0300

    Gobarber BACK-END - Aula 2 - Nodemon & Sucrase
    yarn add sucrase nodemon -D

--- gobarber/src/app.js
+++ gobarber/src/app.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const routes = require('./routes')
+import express from 'express';
+import routes from './routes';
+
 class App {
   constructor() {
     this.server = express();
@@ -18,4 +19,4 @@ class App {
 
 }
 
-module.exports = new App().server;
+export default new App().server;
