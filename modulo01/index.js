54b215e041e99d1169ef7336b4b3fbb35f64519f
Gabriel Jony <gabjony@gmail.com>
2019-11-19 23:08:36 -0300

    Gobarber - Aula 6 - Query & Route params

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const server = express();
 
+//Query params = ?teste=1
+//Route params = /users/1
+//Request boby = { "name": "Gabriel", "email": "gabriel_jony@hotmail.com"}
+
 server.get("/teste", (req, res) => {
-  return res.json({ message: "Hello World!" });
+  const nome = req.query.nome;
+  return res.json({ message: `Hello ${nome}!` });
+});
+
+server.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+  return res.json({ message: `Buscando o usuário ${id}` });
 });
 
 server.listen(3000);
