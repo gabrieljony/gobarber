9465a922a50c225c57e2a3de984332da130b963a
Gabriel Jony <gabjony@gmail.com>
2019-11-20 00:19:56 -0300

    Gobarber - Aula 9 - CRUD - Create, Read, Update, Delete

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const server = express();
 
-const users = ['Gabriel', 'Diego', 'Pedro', 'Victor', 'José']
+server.use(express.json());
+
+const users = ["Gabriel", "Diego", "Pedro", "Victor", "José"];
 
 //Query params = ?teste=1
 //Route params = /users/1
@@ -13,9 +15,44 @@ server.get("/teste", (req, res) => {
   return res.json({ message: `Hello ${nome}!` });
 });
 
+//CRUD - Create, Read, Update, Delete
+
+//Busca por todos usuários
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+//Busca por usuário, passando como parâmetro o id
 server.get("/user/:id", (req, res) => {
   const { id } = req.params;
   return res.json(users[id]);
+});
+
+//Criação de um novo usuário
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+//Edição de um usuário já cadastrado
+server.put("/user/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  users[id] = name;
+  return res.json(users);
+});
+
+//Deletar um usuário
+server.delete("/user/:id", (req, res) => {
+  const { id } = req.params;
+
+  users.splice(id, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
