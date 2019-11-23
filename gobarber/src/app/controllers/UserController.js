021eacba3a7956f40e54ed26b4e4c45da32d9f3e
Gabriel Jony <gabjony@gmail.com>
2019-11-22 23:47:02 -0300

    Gobarber BACK-END - Aula 17 - Validando dados de entrada
    yarn add yup
    Realizado a validação de usuário, na criação e no update do usuário. Validação na sessão da aplicação.

--- gobarber/src/app/controllers/UserController.js
+++ gobarber/src/app/controllers/UserController.js
@@ -1,7 +1,25 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    //Validação com o Yup
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+    });
+
+    //Se der erro na validação status 400
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const userExists = await User.findOne({
       where: {
         email: req.body.email,
@@ -21,7 +39,28 @@ class UserController {
     });
   }
 
+  //EDIÇÃO do usuário
   async update(req, res) {
+    //Validação com o Yup
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) => {
+          return oldPassword ? field.required() : field;
+        }),
+      confirmPassword: Yup.string().when('password', (password, field) => {
+        return password ? field.required().oneOf([Yup.ref('password')]) : field;
+      }),
+    });
+
+    //Se der erro na validação status 400
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
